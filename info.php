20532574cddcb162e9c4463a621342beabcadce7
Marek <marek.stryjenski@gmail.com>
2021-05-08 12:30:19 +0200

    Update info.php

--- info.php
+++ info.php
@@ -4,12 +4,15 @@
 
 $username = "root";
 $passwd = "X34G8gjNabFkcq";
-$hostName = "vmi566217.contaboserver.net";
+// $hostName = "vmi566217.contaboserver.net";
+$hostName = "localhost:3306";
 $dbName = "fruits";
 
 $conn = mysqli_connect($hostName, $username, $passwd, $dbName);
 
 if (!$conn) {
-    echo "cos";
+    echo "cosfsafs";
     echo "connection not working";
+    $sql = "SELECT * FROM fruits";
+    // mysqli_query($sql);
 }
