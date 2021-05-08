9a19888aae3ac6f4ae77eea6055dc8e863f99b7d
Marek <marek.stryjenski@gmail.com>
2021-05-08 12:17:01 +0200

    connectingDb

--- info.php
+++ info.php
@@ -1,1 +1,14 @@
-<?php phpinfo(); ?>
+<?php
+//  phpinfo();
+
+
+$username = "root";
+$passwd = "X34G8gjNabFkcq";
+$hostName = "127.0.0.1";
+$dbName = "contaboServer";
+
+$conn = mysqli_connect($hostName, $username, $passwd, $dbName);
+
+if (!$conn) {
+    echo "connection not working";
+}
