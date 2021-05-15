790cbd376367c2811198997856309a8c3d3fd70b
vladatman <55131890+vladatman@users.noreply.github.com>
2021-05-15 18:31:22 +0300

    Added header and footer of webpage
    Deleted unnecessary files

---
+++ webpage/script.js
@@ -0,0 +1,10 @@
+function changeItem() {
+    document.getElementById("fireFly").style.color = '#00F4C8';
+    document.getElementById("trinityLogo").style.backgroundImage = "url(images/iota-miota-logo-blue.png)";
+}// функция, которая сработает при наведении.
+//она означает - выбрать элемент к Id у которого надо что-то изменить.
+// когда в скобки где написано one-two добавите Id своего элемента
+function rechangeItem() {
+    document.getElementById("fireFly").style.color = '#FFFFFF';
+    document.getElementById("trinityLogo").style.backgroundImage = "url(images/iota-miota-logo.png)";
+}// тут всё также. но наобарот. протсес происходящий про отводе курсора.
