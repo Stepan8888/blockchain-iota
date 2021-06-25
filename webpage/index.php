27728daa592ea3e2bd052d65bb0dc47c15ad5b48
Artem <58528536+dickusbigus@users.noreply.github.com>
2021-06-25 14:56:10 +0600

    fix to the qr db connection

--- webpage/index.php
+++ webpage/index.php
@@ -131,7 +131,9 @@
     <div id="gray3" onclick="show3('none')"></div>
     <div id="popUp">
         <h2><b>Donate</b></h2>
-        <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=<?php">
+        <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=<?php 
+                getWallet($conn);
+                ?>>
     </div>
 
     <div id="popUp2">
