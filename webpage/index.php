c959fdabb302b5295aba005214c01620fa995dda
Artem <58528536+dickusbigus@users.noreply.github.com>
2021-06-27 20:18:59 +0600

    removed ;

--- webpage/index.php
+++ webpage/index.php
@@ -145,7 +145,7 @@
                 //echo '<b>Wallet #'. $counter.'</b>';
                 
                 echo '<span>
-                $row["id"];
+                $row["id"]
                 <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=' . $row["wallet_address"] . '">;
                 </span>';
                 
