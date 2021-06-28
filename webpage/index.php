46cb8a42aa645daea705e5d3656a29dc446c8a0b
Artem <58528536+dickusbigus@users.noreply.github.com>
2021-06-28 10:43:32 +0600

    added wallet adress above qr

--- webpage/index.php
+++ webpage/index.php
@@ -146,6 +146,7 @@
                 
                 echo '<span id="qrText">
               <p>Sensor №' . $row["id"].'</p>
+              <p>' . $row["wallet_address"] . '</p>
                 <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=' . $row["wallet_address"] . '">
                 </span>';
                 
