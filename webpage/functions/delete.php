403883f3d96eea5be8dbfb1f8a208580dbfbf22f
vladatman <55131890+vladatman@users.noreply.github.com>
2021-06-30 19:35:18 +0300

    changing location

--- webpage/functions/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-require "connect.php";
-$id = intval($_GET['id']);
-$query = "DELETE FROM sensors WHERE id = '$id'";
-if(mysqli_query($conn, $query)){
-header("Location: ../adminPanel.php");
-}
-$conn->close();
-?>
